60efd9f724c3df5ccf10eb11d18f261b316b4092
Jenny Fan <40123244+jfmath04@users.noreply.github.com>
2026-02-11 21:45:44 -0500

    Stub out event views

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
+from hoagiecalendar.api.event_views import EventView, EventDetailView
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("event/", EventView.as_view(), name="event"),
+    path("event/<int:event_id>/", EventDetailView.as_view(), name="event-detail"),
 ]
