60bcb2a38ec529bd6a3552447e3c2aa0141a4dc7
Angela Cai <angcai@princeton.edu>
2026-02-25 17:20:51 -0500

    DEV-581 Event Model Serializer (#16)
    * Added event model sterilizer
    
    * added error messages and field names
    
    * changed to like mail

--- backend/hoagiecalendar/api/event_views.py
+++ backend/hoagiecalendar/api/event_views.py
@@ -1,6 +1,42 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import serializers
+from ..models.event import Event
 
+class EventSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(
+		max_length=100,
+		min_length=3,
+		error_messages={
+			"required": "Name must be at least 3 characters.",
+			"min_length": "Name must be at least 3 characters.",
+			"max_length": "Name must be at most 100 characters.",
+		},
+	)
+    location = serializers.CharField(
+		max_length=100,
+		min_length=3,
+		error_messages={
+			"required": "Location must be at least 3 characters.",
+			"min_length": "Location must be at least 3 characters.",
+			"max_length": "Location must be at most 100 characters.",
+		},
+	)
+    host = serializers.CharField(
+		max_length=100,
+		min_length=3,
+		error_messages={
+			"required": "Host must be at least 3 characters.",
+			"min_length": "Host must be at least 3 characters.",
+			"max_length": "Host must be at most 100 characters.",
+		},
+	)
+    class Meta:
+        model = Event
+        fields = ['start', 'end', 'name', 'location', 'description', 
+                  'host', 'owner', 'category', 'from_mail']
+        read_only_fields = ['owner']
+        
 
 class EventView(APIView):
     def get(self, request) -> Response:
