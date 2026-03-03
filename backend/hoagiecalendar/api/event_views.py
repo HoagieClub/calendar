56985f6faf61e49a2c321b3a8b33d2b2b0e192a9
Angela Cai <angcai@princeton.edu>
2026-03-03 14:40:59 -0500

    get put delete

--- backend/hoagiecalendar/api/event_views.py
+++ backend/hoagiecalendar/api/event_views.py
@@ -1,7 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import serializers
+from rest_framework import serializers, status
 from ..models.event import Event
+from django.shortcuts import get_object_or_404
 
 class EventSerializer(serializers.ModelSerializer):
     name = serializers.CharField(
@@ -51,12 +52,20 @@ class EventView(APIView):
 class EventDetailView(APIView):
     def get(self, request, event_id) -> Response:
         # Logic to get details of an event
-        pass
+        event = get_object_or_404(Event, pk=event_id)
+        serializer = EventSerializer(event)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, event_id) -> Response:
         # Logic to update details of an event
-        pass
+        event = get_object_or_404(Event, pk=event_id)
+        serializer = EventSerializer(event, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request, event_id) -> Response:
         # Logic to delete an event
-        pass
+        event = get_object_or_404(Event, pk=event_id)
+        event.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
