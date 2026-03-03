26b242ce302e94681ddfadd5170fde9163df7cd2
Jenny Fan <40123244+jfmath04@users.noreply.github.com>
2026-03-03 01:41:16 -0500

    DEV-639 Backend Lint Workflow (#22)
    * Add Ruff to dev dependencies and run linting
    
    * Linting workflow for backend

--- backend/hoagiecalendar/api/event_views.py
+++ backend/hoagiecalendar/api/event_views.py
@@ -1,10 +1,12 @@
+from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import serializers
+
 from ..models.event import Event
 
+
 class EventSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(
+	name = serializers.CharField(
 		max_length=100,
 		min_length=3,
 		error_messages={
@@ -13,7 +15,7 @@ class EventSerializer(serializers.ModelSerializer):
 			"max_length": "Name must be at most 100 characters.",
 		},
 	)
-    location = serializers.CharField(
+	location = serializers.CharField(
 		max_length=100,
 		min_length=3,
 		error_messages={
@@ -22,7 +24,7 @@ class EventSerializer(serializers.ModelSerializer):
 			"max_length": "Location must be at most 100 characters.",
 		},
 	)
-    host = serializers.CharField(
+	host = serializers.CharField(
 		max_length=100,
 		min_length=3,
 		error_messages={
@@ -31,32 +33,32 @@ class EventSerializer(serializers.ModelSerializer):
 			"max_length": "Host must be at most 100 characters.",
 		},
 	)
-    class Meta:
-        model = Event
-        fields = ['start', 'end', 'name', 'location', 'description', 
-                  'host', 'owner', 'category', 'from_mail']
-        read_only_fields = ['owner']
-        
+
+	class Meta:
+		model = Event
+		fields = ["start", "end", "name", "location", "description", "host", "owner", "category", "from_mail"]
+		read_only_fields = ["owner"]
+
 
 class EventView(APIView):
-    def get(self, request) -> Response:
-        # Logic to get events
-        pass
+	def get(self, request) -> Response:
+		# Logic to get events
+		pass
 
-    def post(self, request) -> Response:
-        # Logic to create an event
-        pass
+	def post(self, request) -> Response:
+		# Logic to create an event
+		pass
 
 
 class EventDetailView(APIView):
-    def get(self, request, event_id) -> Response:
-        # Logic to get details of an event
-        pass
+	def get(self, request, event_id) -> Response:
+		# Logic to get details of an event
+		pass
 
-    def put(self, request, event_id) -> Response:
-        # Logic to update details of an event
-        pass
+	def put(self, request, event_id) -> Response:
+		# Logic to update details of an event
+		pass
 
-    def delete(self, request, event_id) -> Response:
-        # Logic to delete an event
-        pass
+	def delete(self, request, event_id) -> Response:
+		# Logic to delete an event
+		pass
