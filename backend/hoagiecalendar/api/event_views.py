5253dae71420eb76294a60ac9b08f551c04df9ae
celloii <133549470+celloii@users.noreply.github.com>
2026-03-06 11:56:46 -0500

    added check if serializer isvalid

--- backend/hoagiecalendar/api/event_views.py
+++ backend/hoagiecalendar/api/event_views.py
@@ -5,7 +5,7 @@ from rest_framework import serializers, status
 from ..models.event import Event
 
 class EventSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(
+	name = serializers.CharField(
 		max_length=100,
 		min_length=3,
 		error_messages={
@@ -14,7 +14,7 @@ class EventSerializer(serializers.ModelSerializer):
 			"max_length": "Name must be at most 100 characters.",
 		},
 	)
-    location = serializers.CharField(
+	location = serializers.CharField(
 		max_length=100,
 		min_length=3,
 		error_messages={
@@ -23,7 +23,7 @@ class EventSerializer(serializers.ModelSerializer):
 			"max_length": "Location must be at most 100 characters.",
 		},
 	)
-    host = serializers.CharField(
+	host = serializers.CharField(
 		max_length=100,
 		min_length=3,
 		error_messages={
@@ -32,35 +32,36 @@ class EventSerializer(serializers.ModelSerializer):
 			"max_length": "Host must be at most 100 characters.",
 		},
 	)
-    class Meta:
-        model = Event
-        fields = ['start', 'end', 'name', 'location', 'description', 
-                  'host', 'owner', 'category', 'from_mail']
-        read_only_fields = ['owner']
-        
+	class Meta:
+		model = Event
+		fields = ['start', 'end', 'name', 'location', 'description', 
+				  'host', 'owner', 'category', 'from_mail']
+		read_only_fields = ['owner']
+		
 
 class EventView(APIView):
-    def get(self, request) -> Response:
-        # Logic to get events
-        pass
+	def get(self, request) -> Response:
+		# Logic to get events
+		pass
 
-    def post(self, request) -> Response:
-        # Logic to create an event
-		# deserialize the request data
-        serializer = EventSerializer(data=request.data)
-		# add user info (request.user) and save the model instance.
-        serializer.save(owner=request.user)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+	def post(self, request) -> Response:
+		serializer = EventSerializer(data=request.data)
+		if(serializer.is_valid()):
+			# saving the event with the user as the owner
+			serializer.save(owner=request.user)
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		else:
+			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
   
 class EventDetailView(APIView):
-    def get(self, request, event_id) -> Response:
-        # Logic to get details of an event
-        pass
+	def get(self, request, event_id) -> Response:
+		# Logic to get details of an event
+		pass
 
-    def put(self, request, event_id) -> Response:
-        # Logic to update details of an event
-        pass
+	def put(self, request, event_id) -> Response:
+		# Logic to update details of an event
+		pass
 
-    def delete(self, request, event_id) -> Response:
-        # Logic to delete an event
-        pass
+	def delete(self, request, event_id) -> Response:
+		# Logic to delete an event
+		pass
