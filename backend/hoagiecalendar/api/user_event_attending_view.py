a97a3ad917ca800289bef77f7a491a42a614ce0b
Cryptex-github <64497526+asze17@users.noreply.github.com>
2026-04-28 20:39:03 -0400

    Consolidate method logic, fix convention issue and typo

--- backend/hoagiecalendar/api/user_event_attending_view.py
+++ backend/hoagiecalendar/api/user_event_attending_view.py
@@ -3,42 +3,47 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from hoagiecalendar.models.user import User
+
 from ..models.event import Event
+from .event_views import EventSerializer
+
+
+def add_or_remove_attendee_from_event(event_id: str | None, user: User, add: bool) -> Response:
+	if not event_id:
+		return Response({"error": "invalid request: no event_id provided"}, status=status.HTTP_400_BAD_REQUEST)
+
+	try:
+		event = Event.objects.get(pk=event_id)
+		if add:
+			event.attendees.add(user)
+			serializer = EventSerializer(event)
+
+			return Response({"event": serializer.data}, status=status.HTTP_200_OK)
+		else:
+			event.attendees.remove(user)
+
+			return Response(status=status.HTTP_204_NO_CONTENT)
+	except ValueError:
+		return Response({"error": "invalid event_id"}, status=status.HTTP_400_BAD_REQUEST)
+	except Event.DoesNotExist:
+		return Response({"error": "event not found"}, status=status.HTTP_404_NOT_FOUND)
+	except Exception as _:
+		# TODO: Log exception once logging is set up
+		return Response({"error": "an internal error has occurred"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class UserEventAttendingView(APIView):
 	def post(self, request: Request) -> Response:
-		event_id = request.query_params.get("event_id")
-
-		if not event_id:
-			return Response({"error": "invalid request: no event_id provided"}, status=status.HTTP_400_BAD_REQUEST)
-
-		try:
-			Event.objects.get(pk=event_id).attendees.add(request.user)
-		except Event.DoesNotExist:
-			return Response({"error": "event not found"}, status=status.HTTP_400_BAD_REQUEST)
-		except Exception as e:
-			return Response(
-				{"error": f"an internal error has occured: {e}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-			)
-
-		return Response({"message": "added to event attendees (if not already added)"}, status=status.HTTP_201_CREATED)
+		return add_or_remove_attendee_from_event(
+			event_id=request.query_params.get("event_id"),
+			user=request.user,
+			add=True,
+		)
 
 	def delete(self, request: Request) -> Response:
-		event_id = request.query_params.get("event_id")
-
-		if not event_id:
-			return Response({"error": "invalid request: no event_id provided"}, status=status.HTTP_400_BAD_REQUEST)
-
-		try:
-			Event.objects.get(pk=event_id).attendees.remove(request.user)
-		except Event.DoesNotExist:
-			return Response({"error": "event not found"}, status=status.HTTP_400_BAD_REQUEST)
-		except Exception as e:
-			return Response(
-				{"error": f"an internal error has occured: {e}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-			)
-
-		return Response(
-			{"message": "removed from event attendees (if user was an attendee)"}, status=status.HTTP_201_CREATED
+		return add_or_remove_attendee_from_event(
+			event_id=request.query_params.get("event_id"),
+			user=request.user,
+			add=False,
 		)
